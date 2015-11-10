51d50a9b46890c8b948d8d569b43ce3d18573e9b
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-09 20:12:09 -0800

    updated port

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4686);
