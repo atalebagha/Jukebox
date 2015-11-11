d92a9adda62dfb2e2aa6d80fe9b61b6aaffbe0c7
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-10 21:37:03 -0800

    mainly css updates and addition of style elements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,7 +8,9 @@ var JukeBox = React.createClass({
   },
 
   onAddToQueue: function(song){
-    this.setState({queue: this.state.queue.concat(song)});
+    if (this.state.queue.indexOf(song) === -1) {
+      this.setState({queue: this.state.queue.concat(song)});
+    }
   },
 
   onSearchResults: function(searchResults){
@@ -18,6 +20,7 @@ var JukeBox = React.createClass({
   render: function() {
     return (
       <div id="jukebox" className="center" >
+          <h1 className="logo text-center">Jukebox</h1>
           <SearchBar onSearchResults={this.onSearchResults}/>
           <ResultList searchResults={this.state.searchResults} onAddToQueue={this.onAddToQueue}/>
           <Playlist queue={this.state.queue}/>
@@ -47,12 +50,12 @@ var SearchBar = React.createClass({
   render: function() {
     return (
       <div className="container">
-        <div className="jumbotron">
+        <div>
           <div className="row">
-            <div className="input-group">
-              <input ref="search" type="text" className="form-control" placeholder="Search for..." />
+            <div className="input-group input-group-lg search-bar">
+              <input ref="search" type="text" className="form-control search-input" autoFocus/>
               <span className="input-group-btn">
-                <button onClick={this.handleClick} className="btn btn-primary" type="button"><span className=" glyphicon glyphicon-search">Search</span></button>
+                <button onClick={this.handleClick} className="btn btn-primary" type="button"><span className=" glyphicon glyphicon-search"></span></button>
               </span>
             </div>
           </div>
@@ -66,7 +69,6 @@ var ResultList = React.createClass({
 
   render: function() {
     var resultNodes = this.props.searchResults.map(function (track) {
-      console.log(track);
       return (
         <AudioTrack track={track} onClick={this.props.onAddToQueue}>
         </AudioTrack>
@@ -74,8 +76,8 @@ var ResultList = React.createClass({
     }.bind(this));
 
     return (
-      <div className="container">
-        <div className="row">
+      <div className="container cont-center">
+        <div className="row row-centered">
           {resultNodes}
         </div>
       </div>
@@ -89,11 +91,37 @@ var AudioTrack = React.createClass({
     this.props.onClick(this.props.track);
   },
 
+  handleDoubleClick: function (event) {
+    this.props.onStartDrag(this.props.track);
+  },
+
+  componentDidMount: function(){
+    console.log(this.props.canRemove);
+    $('.item').each(function(){
+      var track = $(this);
+      if(track.parents().index('div.queue') >= 0){
+        console.log(track.parents('.queue'));
+        track.hover(function(){
+          track.find('.removesong').fadeIn()
+        },
+        function () {
+          track.find('.removesong').fadeOut()
+        });
+      }
+    });
+  },
+
   render: function() {
     var track = this.props.track;
     return (
-      <div onClick={this.handleClick} className="col-md-4 col-sm-6 col-lg-2 col-xs-12 jumbo-numbers">
-        {track.title}
+      <div onClick={this.handleClick} className="col-md-offset-4 col-sm-6 col-lg-offset-2 col-xs-12 thumbnail item col-centered">
+        <div>
+          <img className="song thumbnail-image" src={track.artwork_url} />
+        </div>
+        <div className="caption songinfo">
+          {track.title}
+          <button className="btn btn-danger removesong">Remove</button>
+        </div>
       </div>
     );
   }
@@ -103,28 +131,73 @@ var Playlist = React.createClass({
 
   getInitialState() {
       return {
-          player: {iframe:null}
+          currentTrack: {
+            title: 'Add some tunes bro...',
+            stream_url: ''
+          }
       };
   },
 
   playSong: function(track){
-    var track_url = 'http://soundcloud.com/forss/flickermood';
-    SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
-      this.setState({player:oEmbed});
-      console.log(this.state.player);
+    var idx = this.props.queue.indexOf(track);
+    // Moves song to the front of the queue when played
+    if(idx !== 0){
+      this.props.queue.splice(idx, 1);
+      this.props.queue.unshift(track);
+    }
+    this.setState({currentTrack:track});
+  },
+
+  removeSong: function (track) {
+    console.log('remove', this.props.queue.indexOf(track));
+    var idx = this.props.queue.indexOf(track);
+    this.props.queue.splice(idx, 1);
+  },
+
+  componentDidUpdate: function(){
+    console.log('updated');
+
+    if(this.props.queue.length && this.state.currentTrack.stream_url === ''){
+      this.playSong(this.props.queue[0]);
+    }
+  },
+
+  componentDidMount: function(){
+    // Adds event listener to the end of a song
+    $('#player').on('ended', function(e){
+      // Removes ended song and plays the next one
+      this.props.queue.shift();
+      if(this.props.queue[0]){
+        this.playSong(this.props.queue[0]);
+      }
     }.bind(this));
   },
 
   render: function() {
     var playlistNodes = this.props.queue.map(function (track) {
           return (
-            <AudioTrack track={track} onClick={this.playSong} />
+            <AudioTrack className='playlistitem' track={track} onClick={this.playSong} canRemove={true}/>
           );
         }.bind(this));
     return (
-      <div className="jumbotron">
+      <div className="queue">
         <div className="container">
-          <div className="player">{this.state.player.iframe}</div>
+          <h4>{this.state.currentTrack.title}</h4>
+          <div className="row">
+            <div className="btn-group col-md-2" role="group" aria-label="...">
+              <button type="button" className="btn btn-default"><span className="glyphicon glyphicon-backward"></span></button>
+              <button type="button" className="btn btn-default"><span className="glyphicon glyphicon-play"></span></button>
+              <button type="button" className="btn btn-default"><span className="glyphicon glyphicon-forward"></span></button>
+            </div>
+            <div className="col-md-8">
+              <div className="progress">
+                  <div className="progress-bar bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+
+                  </div>
+              </div>
+            </div>
+            <audio id='player' src={this.state.currentTrack.stream_url + '?client_id=' + SOUND_CLOUD_KEY} controls autoPlay>Audio</audio>
+          </div>
           <div className="row">
             {playlistNodes}
           </div>
